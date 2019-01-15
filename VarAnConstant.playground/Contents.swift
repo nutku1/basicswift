cc449cb8dfb4f5c06d7f877ac1be9f4e1d1b4890
Supasit.s <asd@com.com>
2019-01-15 09:38:28 +0700

    เริ่มต้นการเขียน swift กำหนด ตัวแปรต่างๆ 15-16  jan

---
+++ VarAnConstant.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+//การประกาศตัวแปร และค่าคงที่
+//Implicit Variable  คือ การประกาศตัวแปรที่ เปลี่ยนแปลงค่าได้ และใช้ DataType จากค่าเริ่มต้นมากำหนด ชนิดของตัวแปร
+var nameString = "Nuutt"
+
+nameString = "Superman"
+
+var number1 = 123
+
+//Explicit Variable
+var surName:String = "Singboon"
+var number2:Int = 555
+var number3:Double = 12.34
+var status1:Bool = true
+
+
+
+
+
