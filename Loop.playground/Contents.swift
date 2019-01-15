328f098418176273c3fd7f75588bd7518f236a5a
Supasit.s <asd@com.com>
2019-01-15 11:52:09 +0700

    การวนรูปในภาษา swift เรียบร้อยแล้ว

---
+++ Loop.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+let numbers = [11,22,33,44,55,66,77,88,99,00]
+
+for testNumber in numbers {
+    print("testNumber ==> \(testNumber)")
+}
+
+var  count:Int = 0
+while count <= 10 {
+    print("count ==> \(count)")
+    count += 1
+}
+
